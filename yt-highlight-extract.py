fe6a74330dd70d3f09ddd325d09f58103d6a62f6
bmunderscore <122417386+bmunderscore@users.noreply.github.com>
2023-09-16 14:05:44 -0400

    Update yt-highlight-extract.py
    converts the mp4 to an audio file, and added a speech recognition module to transcribe the audio to text

--- yt-highlight-extract.py
+++ yt-highlight-extract.py
@@ -5,12 +5,29 @@ import re
 from http.client import IncompleteRead
 from pytube import YouTube
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import *
+import speech_recognition as sr
+
+def transcribe_audio_sphinx(audio_path):
+    recognizer = sr.Recognizer()
+    
+    with sr.AudioFile(audio_path) as source:
+        audio_data = recognizer.record(source)
+    
+    try:
+        transcription = recognizer.recognize_sphinx(audio_data)
+        return transcription
+    except sr.UnknownValueError:
+        print("Sphinx could not understand the audio.")
+    except sr.RequestError:
+        print("Sphinx error; check if you've installed the CMU Sphinx dependencies.")
 
 
 def extract_youtube_id(youtube_url):
-    """regex to extract the YouTube video ID from a URL."""
+    """Extract the YouTube video ID from a URL."""
     match = re.search(r"v=([a-zA-Z0-9_-]{11})", youtube_url)
     return match.group(1) if match else None
+
 
 def get_most_replayed_section(youtube_id):
     url = f"https://yt.lemnoslife.com/videos?part=mostReplayed&id={youtube_id}"
@@ -18,8 +35,11 @@ def get_most_replayed_section(youtube_id):
     data = response.json()
 
     # Check for items in the API response
-    if not data.get('items'):
-        raise ValueError("API response does not contain 'items'")
+# Check for mostReplayed in the first item
+    if not data['items'][0].get('mostReplayed'):
+        print("The video doesn't have sufficient 'mostReplayed' data. This could be due to insufficient views or it being a new video.")
+        return None, None
+
 
     # Check for mostReplayed in the first item
     if not data['items'][0].get('mostReplayed'):
@@ -38,16 +58,15 @@ def get_most_replayed_section(youtube_id):
 
     return start_time_millis / 1000, end_time_millis / 1000  # Convert to seconds
 
-# Downloads the video into a temp file, extracts the highlighted time range, and deletes the temp video
-def download_video_section(youtube_url, start_time, end_time, output_filename, max_retries=3):
+
+def download_video_section(youtube_id, start_time, end_time, output_filename, max_retries=3):
     retries = 0
     while retries < max_retries:
         try:
-            yt = YouTube(f"{youtube_url}")
+            yt = YouTube(f"https://www.youtube.com/watch?v={youtube_id}")
             stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
             download_path = stream.download(filename="temp_video")
             ffmpeg_extract_subclip(download_path, start_time, end_time, targetname=output_filename)
-            # removes temp file when done
             os.remove("temp_video")
             return
         except IncompleteRead:
@@ -57,19 +76,38 @@ def download_video_section(youtube_url, start_time, end_time, output_filename, m
 
     print("Failed to download after multiple attempts.")
 
+def extract_audio_from_video(video_path, output_audio_path="temp_audio.wav"):
+    clip = VideoFileClip(video_path)
+    clip.audio.write_audiofile(output_audio_path, codec='pcm_s16le')  # PCM 16-bit format is compatible
+
+video_path = output_filename
+audio_path = "temp_audio.wav"
+
 def main():
-    # Reads user input for a YouTube URL
     youtube_url = input("Enter the entire YouTube video URL: ")
     youtube_id = extract_youtube_id(youtube_url)
     if not youtube_id:
         print("Invalid YouTube URL provided.")
         return
-    #print(youtube_id)
-    # Reads user input for mp4 file output
+    print(f"Extracted YouTube ID: {youtube_id}")
     output_filename = input("Enter the desired output filename (e.g. output_clip.mp4): ")
-    start_time, end_time = get_most_replayed_section(youtube_url)
+    
+    try:
+        start_time, end_time = get_most_replayed_section(youtube_id)
+        download_video_section(youtube_id, start_time, end_time, output_filename)
+        extract_audio_from_video(video_path, audio_path)
 
-    download_video_section(youtube_id, start_time, end_time, output_filename)
+        transcription = transcribe_audio_sphinx(audio_path)
+
+        # Write the transcription to a text file
+        with open(youtube_id + "-highlight-transcription.txt", "w") as f:
+            f.write(transcription)
+    except ValueError as e:
+        if str(e) == "API response does not contain 'items'":
+            print("The video might not be available or indexed by the API, or the YouTube ID extracted is invalid.")
+        else:
+            print(e)
+
 
 if __name__ == "__main__":
     main()
